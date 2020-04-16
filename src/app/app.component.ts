f08af06bfa033cda68853060c409d2cfe4fd6154
hanskrisnajana123 <hanskrisnajana@ymail.com>
2020-04-16 18:43:31 +0700

    2 files modified - if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   name = "Bro";
+
+  item = [{
+    nama : "Buah Keju",
+    harga : 10000
+  }, {
+    nama : "Buah Semangka",
+    harga : 30000
+  }]
+
+  itemArr = [1,2,3,4]
+
+  show : boolean = true
+
+  user = {
+    nama : "Adm00n"
+
+  }
 }
